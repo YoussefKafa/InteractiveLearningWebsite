5d453bc99b0e359570ad0d931f60d05bdacf70ee
YoussefKafa <48362996+YoussefKafa@users.noreply.github.com>
2020-12-23 14:27:46 +0200

    Dealing With CSS

--- JQueryScripts.js
+++ JQueryScripts.js
@@ -1,5 +1,5 @@
-//click event (hide/show/toggle)-----------------------------------------------------------------------
 $(document).ready(function() {
+    //click event (hide/show/toggle)-----------------------------------------------------------------------
     $("#hideButton").click(function() {
         $("#pToHideAndShow").hide();
     });
@@ -9,32 +9,23 @@ $(document).ready(function() {
     $("#toggleButton").click(function() {
         $("#pToHideAndShow").toggle();
     });
-});
-//double click event-----------------------------------------------------------------------
-$(document).ready(function() {
+    //double click event-----------------------------------------------------------------------
+
     $("#doubleClickButton").dblclick(function() {
         $("#colorParagraph").css("color", "yellow");
     });
-});
-//mouseenter and mouseleave events-----------------------------------------------------------------------
-$(document).ready(function() {
+    //mouseenter and mouseleave events-----------------------------------------------------------------------
     $("#mouseEnterLeave").mouseenter(function() { $(this).css("color", "yellow") });
     $("#mouseEnterLeave").mouseleave(function() { $(this).css("color", "brown") });
-});
-//hover event-------------------------------------------------------------------------------------------------------
-$(document).ready(function() {
+    //hover event-------------------------------------------------------------------------------------------------------
     $("#pWithHover").hover(function() { $(this).css("color", "yellow") }, function() { $(this).css("color", "brown") });
-});
-//callback after an event (with speed effect)-----------------------------------------------------------------------
-$(document).ready(function() {
+    //callback after an event (with speed effect)-----------------------------------------------------------------------
+
     $("#callBackButton").click(function() {
         $("#pCallBack").hide(4000, function() {
             $("#hiddenSpan").show();
         });
     });
-});
-// Working With HTML elements and attributes ----------------------------------------------------------------------------
-$(document).ready(function() {
     $("#changeVal").click(function() {
         $("#textToAlert").attr("value", "value attribute has changed");
     });
@@ -43,16 +34,27 @@ $(document).ready(function() {
         $("#pToWrite").html(x);
     });
 
-});
-//append,prepend,before,after
-$(document).ready(function() {
+    // Working With HTML elements and attributes ----------------------------------------------------------------------------
+
+    //append,prepend,before,after
     $("#append").click(function() { $("#appendDiv").append("<p>append</p>"); });
     $("#prepend").click(function() { $("#appendDiv").prepend("<p>prepend</p>"); });
     $("#before").click(function() { $("#appendDiv").before("<p>before</p>"); });
     $("#after").click(function() { $("#appendDiv").after("<p>after</p>"); });
-});
-//Remove, Empty
-$(document).ready(function() {
+    //Remove, Empty
     $("#removeBtn").click(function() { $("#removeP").remove() });
     $("#emptyBtn").click(function() { $("#emptyP").empty() });
+    //CSS addClass , removeClass
+    $("#turnBlackBtn").click(function() {
+        $("h2").addClass("blkClass");
+    });
+    $("#turnBlueBtn").click(function() {
+        $("h2").removeClass("blkClass");
+    });
+    //CSS get/set
+    $("#clrInputBtn").click(function() {
+        var color = $("#clrInput").val();
+        $("#clrPara").css("color", color);
+    });
+
 });
